5868d710675f8e719949eab6da9fea1145aca3d0
Vlad <vladstoroschuk@gmail.com>
2023-02-17 00:19:07 +0200

    Updated Queries and JavaDoc

--- src/main/java/com/epam/esm/utils/SqlQueries.java
+++ src/main/java/com/epam/esm/utils/SqlQueries.java
@@ -5,10 +5,16 @@ package com.epam.esm.utils;
  * @author Vlad Storoshchuk
  * */
 public class SqlQueries {
+    /**
+     * Class with queries for Order repository
+     * */
     public static class OrdersQueries {
         public static final String GET_ORDERS_BY_USER_ID = "select o from Order o where o.user.id = :user_id";
     }
 
+    /**
+     * Class with queries for Tag repository
+     * */
     public static class TagsQueries {
         public static final String GET_MOSTLY_USED_TAG = """
                 select t.id, t.name from orders ors
@@ -28,11 +34,19 @@ public class SqlQueries {
         public static final String IS_TAG_EXISTS = "select (count(t)> 0) from Tag t where t.name = ?1";
     }
 
+    /**
+     * Class with queries for GiftCertificate repository
+     * */
     public static class GiftCertificatesQueries {
         public static final String IS_GIFT_CERTIFICATE_EXIST = "select (count(g) > 0) from GiftCertificate g where g.name = ?1";
-        public static final String GET_GIFT_CERTIFICATE_BY_TAG_NAME = "SELECT gc.id , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date FROM gift_certificate_tags gct  JOIN tag t JOIN gift_certificate gc  WHERE gc.id = gct.gift_certificate_id AND t.id = gct.tags_id AND t.name = ?1 LIMIT ?2,?3";
+        public static final String GET_GIFT_CERTIFICATE_BY_TAG_NAME = "" +
+                "SELECT gc.id , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date " +
+                "FROM gift_certificate_tags gt  " +
+                "JOIN tag t " +
+                "JOIN gift_certificate gc  " +
+                "WHERE gc.id = gt.gift_certificate_id AND t.id = gt.tags_id AND t.name = ?1 LIMIT ?2,?3";
         public static final String GET_GIFT_CERTIFICATE_BY_PART_OF_DESCRIPTION = "SELECT * FROM gift_certificate WHERE description LIKE ?1 LIMIT ?2,?3";
-        public static final String GET_ALL_GIFT_CERTIFICATES_BY_TAGS_ = "Select g from GiftCertificate g left join fetch g.tags t where t.name in :tagSet";
+        public static final String GET_ALL_GIFT_CERTIFICATES_BY_TAGS_HSQL = "Select g from GiftCertificate g left join fetch g.tags t where t.name in :tagSet";
         public static final String GET_ALL_GIFT_CERTIFICATES_BY_TAGS = """
                 SELECT DISTINCT (gc.id) , gc.name , gc.description, gc.price, gc.duration ,gc.create_date ,gc.last_update_date
                                 FROM gift_certificate_tags gct\s
